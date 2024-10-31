e3473cfa15bcdabfec72a99b8e64247354c440b1
Joachim Horshauge <joachimhorshauge@Joachims-MacBook-Pro.local>
2024-10-31 15:30:55 +0100

    setup home-manager

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
-  description = " Personal Darwin system flake";
+  description = "Personal Darwin system flake";
 
+  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
@@ -8,128 +9,100 @@
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew}:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
   let
     configuration = { pkgs, config, ... }: {
-	nixpkgs.config.allowUnfree = true;
-	security.pam.enableSudoTouchIdAuth = true;
-	nix.extraOptions = ''
-  	  extra-platforms = x86_64-darwin aarch64-darwin
-	'';
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-	pkgs.mkalias
-	pkgs.neovim
-	pkgs.git
-	pkgs.tmux
-	pkgs.alacritty
-	pkgs.obsidian
-        ];
-
-	homebrew = {
-	  enable = true;
-	  brews = [
-	    "mas"
-	  ];
-	  casks = [
-	    "zen-browser"
-	    "fork"
-	    "raycast"
-	  ];
-	  onActivation.cleanup = "zap";
-	};
-
-	fonts.packages = [
-	(pkgs.nerdfonts.override { fonts = ["JetBrainsMono"];})
-	];
-
-system.activationScripts.applications.text = let
-  env = pkgs.buildEnv {
-    name = "system-applications";
-    paths = config.environment.systemPackages;
-    pathsToLink = "/Applications";
-  };
-in
-  pkgs.lib.mkForce ''
-  # Set up applications.
-  echo "setting up /Applications..." >&2
-  rm -rf /Applications/Nix\ Apps
-  mkdir -p /Applications/Nix\ Apps
-  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-  while read src; do
-    app_name=$(basename "$src")
-    echo "copying $src" >&2
-    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-  done
+      nixpkgs.config.allowUnfree = true;
+      security.pam.enableSudoTouchIdAuth = true;
+      nix.extraOptions = ''
+        extra-platforms = x86_64-darwin aarch64-darwin
       '';
+
+      # System packages
+      environment.systemPackages = [
+        pkgs.mkalias
+        pkgs.neovim
+        pkgs.git
+        pkgs.tmux
+        pkgs.alacritty
+        pkgs.obsidian
+      ];
+
+      homebrew = {
+        enable = true;
+        brews = [ "mas" ];
+        casks = [ "zen-browser" "fork" "raycast" "wireshark" ];
+        onActivation.cleanup = "zap";
+      };
+
+      fonts.packages = [
+        (pkgs.nerdfonts.override { fonts = ["JetBrainsMono"]; })
+      ];
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       system.defaults = {
         dock.autohide = true;
-	dock.persistent-apps = [
-	  "${pkgs.alacritty}/Applications/Alacritty.app"
-	  "/Applications/Zen Browser.app"
-	  "${pkgs.obsidian}/Applications/Obsidian.app"
-	  "/System/Applications/Mail.app"
-	  "/System/Applications/Calendar.app"
-	  ];
-	dock.mru-spaces = false;
-	finder.AppleShowAllExtensions = true;
-	finder.FXPreferredViewStyle = "clmv";
-	loginwindow.GuestEnabled = false;
-	loginwindow.LoginwindowText = "Joachim Horshauge - Personal";
-  	screencapture.location = "~/Pictures/screenshots";
-  	screensaver.askForPasswordDelay = 10;
-	NSGlobalDomain.AppleICUForce24HourTime = true;
-	NSGlobalDomain.AppleInterfaceStyle = "Dark";
-	NSGlobalDomain.KeyRepeat = 2;
+        dock.persistent-apps = [
+          "${pkgs.alacritty}/Applications/Alacritty.app"
+          "/Applications/Zen Browser.app"
+          "${pkgs.obsidian}/Applications/Obsidian.app"
+          "/System/Applications/Mail.app"
+          "/System/Applications/Calendar.app"
+        ];
+        dock.mru-spaces = false;
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.GuestEnabled = false;
+        loginwindow.LoginwindowText = "Personal";
+        screencapture.location = "~/Pictures/screenshots";
+        screensaver.askForPasswordDelay = 10;
+        NSGlobalDomain.AppleICUForce24HourTime = true;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+        NSGlobalDomain.KeyRepeat = 2;
+        NSGlobalDomain._HIHideMenuBar = true;
+        trackpad.Clicking = true;
       };
 
-      # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
+      programs.zsh.enable = true;
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."personal" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-      configuration 
-      nix-homebrew.darwinModules.nix-homebrew
-        {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew {
           nix-homebrew = {
             enable = true;
-
             enableRosetta = true;
-
             user = "joachimhorshauge";
-          
-        };    
-    }
-    ];
+          };
+        }
+      ];
     };
 
-    # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."personal".pkgs;
   };
 }
