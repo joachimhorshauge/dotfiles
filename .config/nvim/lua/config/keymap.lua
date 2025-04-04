cd91ecd4ca0a8d8a8f83940a570666a1a6bfb1eb
Joachim Horshauge <work@joachimhorshauge.com>
2025-04-04 14:56:56 +0200

    [Added] Formatting and completion

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,2 +1,10 @@
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open parent directory in Oil"})
-vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, {desc="Open diagnostics in a floating window"})
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open parent directory in Oil" })
+vim.keymap.set("n", "gl", function()
+	vim.diagnostic.open_float()
+end, { desc = "Open diagnostics in a floating window" })
+
+vim.keymap.set("n", "<leader>cf", function()
+	require("conform").format({
+		lsp_format = "fallback",
+	})
+end, { desc = "Format current file" })
